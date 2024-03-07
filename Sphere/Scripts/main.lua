3530c9a21df65a4e1fd2105abeef89b17a1bba7e
DecioLuvier <decioluvieriii@gmail.com>
2024-03-07 08:51:23 -0300

    Update main.lua

--- Sphere/Scripts/main.lua
+++ Sphere/Scripts/main.lua
@@ -2,7 +2,7 @@ local Logger = require("libs/Logger")
 Logger.Initialize()
 local DataManager = require("DataManager/main")
 
-Logger.print("Version: V2.3 03/07/2024")
+Logger.print("Version: V2.4 03/07/2024")
 Logger.print("Starting...")
 
 SphereGlobal = {}
@@ -22,7 +22,7 @@ if validDataManager then
         Logger.print("Creator: Decio Luvier (#pw_another)")
         Logger.print("Special thanks to Patreon supporters:")
         Logger.print("#minhon.")
-        Logger.print("#proj1w420.")
+        Logger.print("#proj1w420")
         Logger.print("https://discord.gg/cS62fmW6TF")
         Logger.print("--------")
     end
